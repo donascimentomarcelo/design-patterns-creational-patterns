f1e6998396e35a4b9719d3d30b0ad1dbd6cd4e1c
Marcelo Sant'Anna <marcelojunin2010@hotmail.com>
2022-08-26 21:39:02 -0300

    Implementing Factory Method

--- src/main/java/br/com/cod3r/factory/apple/Client.java
+++ src/main/java/br/com/cod3r/factory/apple/Client.java
@@ -1,11 +1,18 @@
 package br.com.cod3r.factory.apple;
 
+import br.com.cod3r.factory.apple.factory.IPhoneFactory;
+import br.com.cod3r.factory.apple.model.IPhone;
+
 public class Client {
 	
 	public static void main(String[] args) {
 		
 		System.out.println("### Ordering an iPhone X");
-		
+		final IPhone standard = IPhoneFactory.orderIPhone("X", "standard");
+		System.out.println(standard);
+
 		System.out.println("\n\n### Ordering an iPhone 11 HighEnd");
+		final IPhone highEnd = IPhoneFactory.orderIPhone("11", "highEnd");
+		System.out.println(highEnd);
 	}
 }
